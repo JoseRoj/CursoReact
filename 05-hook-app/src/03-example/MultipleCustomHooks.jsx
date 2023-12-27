a22d6ca4bde092e830337889a7d9c6df08d8b107
JoseRoj <jorojas2019@udec.cl>
2023-12-27 19:20:03 -0300

    📈 Profundización useRef …

---
+++ 05-hook-app/src/03-example/MultipleCustomHooks.jsx
@@ -0,0 +1,35 @@
+import { useCounter, useFetch } from "../Hooks";
+import { LoadingQuote, Quote } from "./index";
+export const MultipleCustomHooks = () => {
+  const { counter, increment } = useCounter(1);
+  const { data, isLoading, hasError } = useFetch(
+    `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
+  );
+  const { author, quote } = !!data && data[0]; //!undefined = true -- !!undefined = false
+  console.log({ data, isLoading, hasError });
+  return (
+    <>
+      <h1>Breaking Bad Hooks</h1>
+      <hr />
+      {/*isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )*/}
+      {isLoading ? <LoadingQuote /> : <Quote author={author} quote={quote} />}
+
+      <button
+        className="btn btn-primary"
+        disabled={isLoading}
+        onClick={() => {
+          increment();
+        }}
+      >
+        Next quote
+      </button>
+    </>
+  );
+};
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -4,10 +4,11 @@ import "./index.css";
 //import { CounterApp } from "./01-useState/CounterApp";
 //import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 //import { SimpleForm } from "./02-useEffect/SimpleForm";
-import { FormWithCustomHook } from "./02-useEffect/SimpleFormCustomHook";
-
+//import { FormWithCustomHook } from "./02-useEffect/SimpleFormCustomHook";
+//import { MultipleCustomHooks } from "./03-example/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-  <FormWithCustomHook />
+  <FocusScreen />
   //</React.StrictMode>
 );
